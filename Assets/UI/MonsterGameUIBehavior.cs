adf0439946b666c6d55ac79ee4e654ee20710aeb
Christopher Angelo <angelo@angeloanan.xyz>
2023-05-22 07:17:03 +0200

    feat: implement mock UI based on Figma design
    Design used: https://www.figma.com/file/7cZ2G40wHUAyDs7gr3Zunl/Untitled?type=design&node-id=0-1&t=swYx2YOGJQxShTyH-0

--- Assets/UI/MonsterGameUIBehavior.cs
+++ Assets/UI/MonsterGameUIBehavior.cs
@@ -1,34 +1,40 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class MonsterGameUIBehavior : MonoBehaviour
-{
-    private void OnEnable() {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+public class MonsterGameUIBehavior : MonoBehaviour {
+  [SerializeField] private GameObject _WorldRoot;
 
-        VisualElement cubeItem = root.Q<VisualElement>("Item");
-        
-        cubeItem.RegisterCallback<MouseEnterEvent>(e => {
-            cubeItem.style.backgroundColor = new StyleColor(new Color(0.8f, 0.8f, 0.8f));
-        });
-        cubeItem.RegisterCallback<MouseLeaveEvent>(e => {
-            cubeItem.style.backgroundColor = new StyleColor(Color.white);
-        });
-    }
-    
+  private bool isDraggingObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  private void OnEnable() {
+    VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    VisualElement cubeItem = root.Q<VisualElement>("Item");
+
+
+    // Hover style
+    cubeItem.RegisterCallback<MouseEnterEvent>(e => {
+      cubeItem.style.backgroundColor = new StyleColor(new Color(0.8f, 0.8f, 0.8f));
+    });
+    cubeItem.RegisterCallback<MouseLeaveEvent>(e => { cubeItem.style.backgroundColor = new StyleColor(Color.white); });
+
+
+    cubeItem.RegisterCallback<ClickEvent>(e => {
+      var prefab = Resources.Load<GameObject>("CubePrefab");
+      var screenCenter = new Vector3(Screen.width / 2, Screen.height / 2, -3);
+      // Project screen center to world
+      var worldCenter = Camera.main.ScreenToWorldPoint(screenCenter);
+
+      Instantiate(prefab, worldCenter, Quaternion.identity);
+    });
+
+    // TODO: Below's code is for item dragging
+    cubeItem.RegisterCallback<PointerDownEvent>(e => { Debug.Log("Pointer Down"); });
+    cubeItem.RegisterCallback<PointerUpEvent>(e => { Debug.Log("Pointer Up"); });
+    // Handle whether should render item
+    cubeItem.RegisterCallback<MouseOutEvent>(e => {
+      // IF mouse is clicking
+      // Then call function, move the object until mouse is released 
+    });
+  }
 }
