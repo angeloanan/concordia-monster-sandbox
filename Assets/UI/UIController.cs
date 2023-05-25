82ddf35a043b0e145f3259af9696773db4552baa
jonastheon7 <jonastheon7@gmail.com>
2023-05-25 22:19:42 +0200

    feat: add sounds asset to Unity
    - Add every audio assets in the one drive to Unity
    - Implement theme song

--- Assets/UI/UIController.cs
+++ Assets/UI/UIController.cs
@@ -6,28 +6,25 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
-using Button = UnityEngine.UIElements.Button;
-
+using Button = UnityEngine.UIElements.Button; 
 public class UIController : MonoBehaviour
 {
     public Button type1Button;
     public TextField type1Text;
-
+    
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
 
-
         type1Button = root.Q<Button>("type1-button");
         type1Text = root.Q<TextField>("type1-text");
-        type1Button.clicked += type1ButtonPressed; 
+        type1Button.clicked += type1ButtonPressed;
     }
 
     void type1ButtonPressed()
     {
         if (type1Text.style.display == DisplayStyle.Flex)
         {
-            
             type1Text.style.display = DisplayStyle.None;
         }
         else
@@ -35,4 +32,5 @@ public class UIController : MonoBehaviour
             type1Text.style.display = DisplayStyle.Flex;
         }
     }
+    
 }
