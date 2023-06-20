d41c5e45f1b33ef54db94b5034e7a8e163ece9d9
Christopher Angelo <angelo@angeloanan.xyz>
2023-06-20 12:06:56 +0200

    feat: assign monster name on continue button click

--- Assets/UI/MonsterName.cs
+++ Assets/UI/MonsterName.cs
@@ -1,19 +1,17 @@
-using System;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
-public class MonsterName : MonoBehaviour
-{
-    private void Awake()
-    {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
-        root.Q<Button>("ContinueButton").clicked += () =>
-        {
-            Debug.Log("Button Clicked");
-            SceneManager.LoadScene("Scenes/GameWorld");
-        };
-        
-    }
+public class MonsterName : MonoBehaviour {
+  private void Awake() {
+    var root = GetComponent<UIDocument>().rootVisualElement;
+    var continueButton = root.Q<Button>("ContinueButton");
+    var monsterNameTextFeld = root.Q<TextField>();
+
+    continueButton.RegisterCallback<ClickEvent>(_ => {
+      MonsterDataManager.Instance.SetCurrentActiveMonsterName(monsterNameTextFeld.value);
+
+      SceneManager.LoadScene("Scenes/GameWorld");
+    });
+  }
 }
