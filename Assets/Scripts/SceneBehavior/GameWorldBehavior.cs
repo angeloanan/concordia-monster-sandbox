a9f2947738e45792ae20d66d78822138a3969a83
Christopher Angelo <angelo@angeloanan.xyz>
2023-06-20 11:38:25 +0200

    refactor: Simplify camera movement and object selection logic
    The commit simplifies the camera movement and object selection logic by removing redundant code, adding comments, and refactoring methods. The changes include:
    - Remove unnecessary debug log
    - Refactor OnStopLooking method to simplify object selection logic
    - Refactor ChangeActiveObject method to remove outline shader and mount UI element from old objects
    - Simplify LateUpdate method by removing redundant if statement for moving camera or object
    - Add comments to clarify each method's purpose

--- Assets/Scripts/SceneBehavior/GameWorldBehavior.cs
+++ Assets/Scripts/SceneBehavior/GameWorldBehavior.cs
@@ -37,43 +37,61 @@ public class GameWorldBehavior : MonoBehaviour {
   /// Could be better represented via a State-Machine of some sort, but idk how to do that and cba to learn.
   /// </summary>
   private void OnStopLooking() {
-    Debug.Log($"Camera did move: {_cameraDidMove}");
     _isClicking = false;
     
     // If an object is selected WHILE camera is being moved, then don't deselect the camera
+    // Do whatever onClick callbacks here
     if (!_cameraDidMove) {
-      _activeObject = null;
-      _cameraDidMove = false;
+      // Raytrace cursor position to world and check if it hits anything
+      // If it hits something, then set the object to be dragged
+      var ray = Camera.main.ScreenPointToRay(_pointerInitialPosition);
+      Physics.Raycast(ray, out var hit, 100);
+      Debug.DrawRay(_pointerInitialPosition, ray.direction * 100, Color.yellow, 30);
+      if (hit.collider != null && hit.transform.CompareTag("Draggable")) {
+        ChangeActiveObject(hit.transform.gameObject);
+      }
     }
+    
+    _cameraDidMove = false;
   }
 
   private void ChangeActiveObject(GameObject newActiveObject) {
+    if (_activeObject == newActiveObject) return;
+    
     if (_activeObject != null) {
       // Cleanup old object
       // 1. Remove outline shader
+      if (_activeObject.TryGetComponent<Outline>(out var outline)) {
+        Destroy(outline);
+      }
+      
       // 2. Remove mounted UI element
-
-      _activeObject = null;
     }
 
     if (newActiveObject != null) {
       // Assign new object to stuff
       // 1. Add outline shader
-      // 2. Mount UI element
+      var outline = newActiveObject.AddComponent<Outline>();
 
-      _activeObject = newActiveObject;
+      outline.OutlineMode = Outline.Mode.OutlineAll;
+      outline.OutlineColor = Color.yellow;
+      outline.OutlineWidth = 5f;
+      // 2. Mount UI element
     }
+    
+    _activeObject = newActiveObject;
   }
 
-  // OnMouseDrag
+  // OnLook - Run every frame
   public void OnPointerDelta(InputAction.CallbackContext ctx) {
     _pointerDelta = ctx.ReadValue<Vector2>();
   }
+  // OnMouseMove - Run every frame
   public void OnPointerInitialPosition(InputAction.CallbackContext ctx) {
     _pointerInitialPosition = ctx.ReadValue<Vector2>();
   }
 
-  // OnClick
+  // OnClick - Run every frame
   public void OnPointerClick(InputAction.CallbackContext ctx) {
     Debug.Assert(Camera.main != null, "Camera.main != null");
 
@@ -85,19 +103,6 @@ public class GameWorldBehavior : MonoBehaviour {
     }
 
     _isClicking = true;
-
-    // Raytrace cursor position to world and check if it hits anything
-    // If it hits something, then set the object to be dragged
-    var ray = Camera.main.ScreenPointToRay(_pointerInitialPosition);
-    Physics.Raycast(ray, out var hit, 100);
-    Debug.DrawRay(_pointerInitialPosition, ray.direction * 100, Color.yellow, 30);
-    if (hit.collider != null && hit.transform.CompareTag("Draggable")) {
-      ChangeActiveObject(hit.transform.gameObject);
-    }
-    else {
-      // Handle camera move
-      _activeObject = null; // Sanity check, shouldn't need this but whatever
-    }
   }
 
   private void LateUpdate() {
@@ -110,26 +115,21 @@ public class GameWorldBehavior : MonoBehaviour {
     // Only handle drag stuff if we're clicking
     if (!_isClicking) return;
 
-    if (_activeObject != null) { // Moving object
-      // TODO: stuff (See ENG-38)
-    }
-    else { // Moving camera
-      if (!_cameraDidMove && _pointerDelta.magnitude > 1f) _cameraDidMove = true;
-      float rotationAroundXAxis = -_pointerDelta.y * cameraSensitivity;
-      float rotationAroundYAxis = _pointerDelta.x * cameraSensitivity;
+    if (!_cameraDidMove && _pointerDelta.magnitude > 1f) _cameraDidMove = true;
+    float rotationAroundXAxis = -_pointerDelta.y * cameraSensitivity;
+    float rotationAroundYAxis = _pointerDelta.x * cameraSensitivity;
 
-      // TP Camera to center position
-      var cam = Camera.main;
-      cam.transform.position = cameraCenter;
-      cam.transform.Rotate(Vector3.right, rotationAroundXAxis);
-      cam.transform.Rotate(Vector3.up, rotationAroundYAxis, Space.World);
+    // TP Camera to center position
+    var cam = Camera.main;
+    cam.transform.position = cameraCenter;
+    cam.transform.Rotate(Vector3.right, rotationAroundXAxis);
+    cam.transform.Rotate(Vector3.up, rotationAroundYAxis, Space.World);
 
-      // Clamp rotation
-      var currentRotation = cam.transform.rotation.eulerAngles;
-      currentRotation.x = Mathf.Clamp(currentRotation.x, -cameraClampRotationDeg, cameraClampRotationDeg);
-      cam.transform.rotation = Quaternion.Euler(currentRotation);
+    // Clamp rotation
+    var currentRotation = cam.transform.rotation.eulerAngles;
+    currentRotation.x = Mathf.Clamp(currentRotation.x, -cameraClampRotationDeg, cameraClampRotationDeg);
+    cam.transform.rotation = Quaternion.Euler(currentRotation);
 
-      cam.transform.Translate(new Vector3(0, 0, -cameraRadiusFromCenter));
-    }
+    cam.transform.Translate(new Vector3(0, 0, -cameraRadiusFromCenter));
   }
 }
